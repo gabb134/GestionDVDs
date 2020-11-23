d188a3b3472fd2087c064a9bbb7b0c2f2db85e84
gabb134 <cg.marrero@cgodin.qc.ca>
2020-11-23 09:41:08 -0500

    modif gab, creation projet avec la 2.1

--- GestionDVDs/GestionDVDs/Data/AppIdentityUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace GestionDVDs.Data
-{
-    public class AppIdentityUser : IdentityUser // on va ajouter dan cette classe
-    {
-
-    }
-}
---
+++ GestionDesDVDs/GestionDesDVDs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestionDesDVDs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
